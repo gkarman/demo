651efdd93cf1bc9b5c51629796f102557dbdbd2f
Григорий Карман <g.karman@mail.ru>
2026-01-21 13:54:06 +0300

    env + makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	@set -a; source .env.local; set +a; go run ./cmd/demo
--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gkarman/demo/internal/config"
+import (
+	"fmt"
+
+	"github.com/gkarman/demo/internal/config"
+)
 
 func main() {
 	cfg := config.MustLoad("configs/config.yaml")
 	_ = cfg
+	fmt.Printf("%+v\n", cfg)
 }

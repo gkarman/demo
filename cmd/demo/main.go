3c6743a43a53ee40200f1801c596cbcd68d547b7
Григорий Карман <g.karman@mail.ru>
2026-01-21 13:14:19 +0300

    go mod init

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,1 +1,8 @@
 package main
+
+import "github.com/gkarman/demo/internal/config"
+
+func main() {
+	cfg := config.MustLoad("configs/config.yaml")
+	_ = cfg
+}
--- internal/config/loader.go
+++ internal/config/loader.go
@@ -9,14 +9,12 @@ import (
 func MustLoad(path string) *Config {
 	var cfg Config
 
-	// 1️⃣ Загружаем файл (если есть)
 	if path != "" {
 		if err := cleanenv.ReadConfig(path, &cfg); err != nil {
 			log.Fatalf("failed to read config file: %v", err)
 		}
 	}
 
-	// 2️⃣ ENV всегда поверх
 	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		log.Fatalf("failed to read env: %v", err)
 	}
